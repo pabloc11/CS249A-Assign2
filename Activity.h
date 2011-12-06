70c4e13e85cd36f837a06e337eab14f82041af9a
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2011-12-05 19:56:54 -0800

    Activity Manager compiles

--- Activity.h
+++ Activity.h
@@ -2,84 +2,79 @@
 #define __ACTIVITY_H__
 
 #include <string>
-
 #include "PtrInterface.h"
 #include "Ptr.h"
-
 #include "Nominal.h"
 #include "fwk/Notifiee.h"
 
 using std::string;
 
 /* Define the type 'Time' */
-class Time : public Ordinal<Time,double> {
-public:
-    Time(double time) : Ordinal<Time,double>(time)
-    {}
+class Time : public Ordinal<Time,double>
+{
+	public:
+		Time() : Ordinal<Time,double>(0.0) {}
+		Time(double time) : Ordinal<Time,double>(time) {}	
 };
 
-class Activity : public Fwk::PtrInterface<Activity> {
- public:
-    typedef Fwk::Ptr<Activity> Ptr;
-    
-    /* Notifiee class for Activities */
- class Notifiee : public Fwk::BaseNotifiee<Activity> {
-    public:
-	typedef Fwk::Ptr<Notifiee> Ptr;
+class Activity : public Fwk::PtrInterface<Activity>
+{
+	public:
+	  typedef Fwk::Ptr<Activity> Ptr;
+  
+	  /* Notifiee class for Activities */
+		class Notifiee : public Fwk::BaseNotifiee<Activity>
+		{
+			public:
+				typedef Fwk::Ptr<Notifiee> Ptr;
+				Notifiee(Activity* act) : Fwk::BaseNotifiee<Activity>(act) {}
+				virtual void onNextTime() {}
+				virtual void onStatus() {}
+		};
 
-        Notifiee(Activity* act) : Fwk::BaseNotifiee<Activity>(act) {}
+	  class Manager;
+	  enum Status {free, waiting, ready, executing, nextTimeScheduled, deleted};
 
-        virtual void onNextTime() {}
-	virtual void onStatus() {}
-    };
+	  virtual Status status() const = 0;
+	  virtual void statusIs(Status s)  = 0;
 
-    class Manager;
+	  virtual Time nextTime() const = 0;
+	  virtual void nextTimeIs(Time t) = 0;
 
-    enum Status {
-        free, waiting, ready, executing, nextTimeScheduled, deleted
-    };
+	  virtual Fwk::Ptr<Notifiee> notifiee() const = 0;
+	  virtual void lastNotifieeIs(Notifiee* n) = 0;
+	  virtual string name() const { return name_; }
 
-    virtual Status status() const = 0;
-    virtual void statusIs(Status s)  = 0;
-    
-    virtual Time nextTime() const = 0;
-    virtual void nextTimeIs(Time t) = 0;
+	protected:
+	  Activity(const string &name) : name_(name) {}
 
-    virtual Fwk::Ptr<Notifiee> notifiee() const = 0;
-
-    virtual void lastNotifieeIs(Notifiee* n) = 0;
-
-    virtual string name() const { return name_; }
-
-protected:
-    Activity(const string &name)
-        : name_(name)
-    {}
-
-private:
-    string name_;
-
+	private:
+	  string name_;
 };
 
-class Activity::Manager : public Fwk::PtrInterface<Activity::Manager> {
-public:
-    typedef Fwk::Ptr<Activity::Manager> Ptr;
+class Activity::Manager : public Fwk::PtrInterface<Activity::Manager>
+{
+	public:
+	 	typedef Fwk::Ptr<Activity::Manager> Ptr;
+		Ptr activityManagerInstance();
 
-    virtual Fwk::Ptr<Activity> activityNew(const string &name) = 0;
+		virtual Fwk::Ptr<Activity> activityNew(const string &name) = 0;
+		virtual Fwk::Ptr<Activity> activity(const string &name) const = 0;
+		
+		virtual void activityDel(const string &name) = 0;
+		virtual void lastActivityIs(Activity::Ptr) = 0;
 
-    virtual Fwk::Ptr<Activity> activity(const string &name) const = 0;
+		virtual Time now() const = 0;
+		virtual void nowIs(Time) = 0;
+};
 
-    virtual void activityDel(const string &name) = 0;
-
-    virtual void lastActivityIs(Activity::Ptr) = 0;
-
-    virtual Time now() const = 0;
-    virtual void nowIs(Time) = 0;
-
-
-private:
-    /* Up to you */
-
+class ActivityComparator
+{
+	public:
+  	bool operator() (const Activity::Ptr lhs, const Activity::Ptr rhs) const
+  	{	
+			return lhs->nextTime().value() < rhs->nextTime().value();
+  	}
 };
 
 extern Fwk::Ptr<Activity::Manager> activityManagerInstance();
--- Makefile
+++ Makefile
@@ -1,25 +1,25 @@
 CPPFLAGS = -I.
 CXXFLAGS = -Wall -g
 
-OBJECTS = Instance.o engine/Entity.o engine/Network.o engine/Stats.o engine/Fleet.o engine/Segment.o engine/TruckSegment.o engine/PlaneSegment.o engine/BoatSegment.o engine/Location.o engine/CustomerLocation.o engine/PortLocation.o engine/TerminalLocation.o engine/TruckTerminal.o engine/BoatTerminal.o engine/PlaneTerminal.o engine/Shipment.o engine/SegmentReactor.o engine/NetworkReactor.o engine/LocationReactor.o engine/CustomerReactor.o
+OBJECTS = ActivityImpl.o Instance.o engine/Entity.o engine/Network.o engine/Stats.o engine/Fleet.o engine/Segment.o engine/TruckSegment.o engine/PlaneSegment.o engine/BoatSegment.o engine/Location.o engine/CustomerLocation.o engine/PortLocation.o engine/TerminalLocation.o engine/TruckTerminal.o engine/BoatTerminal.o engine/PlaneTerminal.o engine/Shipment.o engine/SegmentReactor.o engine/NetworkReactor.o engine/LocationReactor.o engine/CustomerReactor.o
 LIBS = fwk/BaseCollection.o fwk/BaseNotifiee.o fwk/Exception.o
 
 default: test1 example test2 sample
 
-test1:	test1.o $(OBJECTS) $(LIBS)
+test1:	test/test1.o $(OBJECTS) $(LIBS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
-test2: test2.o $(OBJECTS) $(LIBS)
+test2: test/test2.o $(OBJECTS) $(LIBS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
-sample: sample.o $(OBJECTS) $(LIBS)
+sample: test/sample.o $(OBJECTS) $(LIBS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
-example:	example.o $(OBJECTS) $(LIBS)
+example:	test/example.o $(OBJECTS) $(LIBS)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
 clean:
-	rm -f test1 test1.o test2 test2.o sample sample.o example example.o $(OBJECTS) $(LIBS) *~
+	rm -f test1 test/test1.o test2 test/test2.o sample test/sample.o example test/example.o $(OBJECTS) $(LIBS) *~
 
 Instance.o: Instance.cpp Instance.h PtrInterface.h Ptr.h Engine.h
-test1.o: test1.cpp Instance.h PtrInterface.h Ptr.h
+test1.o: test/test1.cpp Instance.h PtrInterface.h Ptr.h
